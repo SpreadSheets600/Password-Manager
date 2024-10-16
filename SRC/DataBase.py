3be5b74c6b88107c5d2cbd2f15799e2b7416b42f
SpreadSheets600 <Sohammaity006@gmail.com>
2024-10-16 13:43:56 +0530

    Added Database Handler

---
+++ SRC/DataBase.py
@@ -0,0 +1,28 @@
+import sqlite3
+from Exceptions import DatabaseError
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('passwords.db')
+        self.create_table()
+
+    def create_table(self):
+        try:
+            with self.conn:
+                self.conn.execute('''CREATE TABLE IF NOT EXISTS passwords (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    website TEXT NOT NULL,
+                                    email TEXT NOT NULL,
+                                    username TEXT NOT NULL,
+                                    password TEXT NOT NULL
+                                    );''')
+        except sqlite3.Error as e:
+            raise DatabaseError(f"Database error: {e}")
+
+    def save_data(self, website, email, username, password):
+        try:
+            with self.conn:
+                self.conn.execute("INSERT INTO passwords (website, email, username, password) VALUES (?, ?, ?, ?)",
+                                  (website, email, username, password))
+        except sqlite3.Error as e:
+            raise DatabaseError(f"Failed to save data: {e}")
