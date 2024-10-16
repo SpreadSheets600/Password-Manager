d82d950e2ceac627977a252f3e5b6f954678710e
SpreadSheets600 <Sohammaity006@gmail.com>
2024-10-16 13:44:52 +0530

    Added Utility Handler

---
+++ SRC/Utilities.py
@@ -0,0 +1,18 @@
+import random
+import pyperclip
+
+def generate_password(entry):
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    symbols = '!@#$%^&*()'
+    
+    password_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    pyperclip.copy(password)
+
+    entry.delete(0, "end")
+    entry.insert(0, password)
