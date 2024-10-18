50a8f2b0ebb045c4ddab5695bea83315312993f9
SpreadSheets600 <Sohammaity006@gmail.com>
2024-10-18 09:40:25 +0530

    Resolved Conflicts
    Co-Authored-By: Rishikeshav Ravichandran <94991501+iblamerishi@users.noreply.github.com>

--- SRC/Utilities.py
+++ SRC/Utilities.py
@@ -1,16 +1,28 @@
 import random
 import pyperclip
 
+
 def generate_password():
-    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    numbers = '0123456789'
-    symbols = '!@#$%^&*()'
-    
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "!@#$%^&*()"
+
     password_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
     password_list += [random.choice(symbols) for _ in range(random.randint(2, 4))]
     password_list += [random.choice(numbers) for _ in range(random.randint(2, 4))]
 
     random.shuffle(password_list)
-    password = ''.join(password_list)
+    password = "".join(password_list)
     pyperclip.copy(password)
     return password
+
+
+def password_strength(password):
+    if len(password) < 6:
+        return "WEAK"
+    up = any(char.isupper() for char in password)
+    digit = any(char.isdigit() for char in password)
+
+    if up and digit:
+        return "STRONG"
+    return "MEDIUM"
